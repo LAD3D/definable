3c107c75fcfe81a3041e363d33d2e005ddd57f57
Sergio Arbeo <serabe@gmail.com>
2010-04-09 00:39:48 +0200

    Refactored definition_spec.
    Furthermore, a bug was detected and killed. It let a class be accepted
    as an object.

--- spec/definable/definition_spec.rb
+++ spec/definable/definition_spec.rb
@@ -4,12 +4,13 @@ module Definable
 
   describe Definition do
     before(:each) do
-      @result_class = Class.new do
-                        def initialize(*args)
-                        end
-                      end
+      @result_class = Class.new do;def initialize(*args);end;end
 
-      @params = [Class.new, Class.new]
+      classes = [Class.new, Class.new]
+      tags = [:parallel, :normal]
+      @ready_params = classes.map(&:new).zip(tags)
+      @params = classes.zip(tags)
+                      
       @definition = Definable::Definition.new(@params, @result_class, [:parallel, :normal])
       @owner =  Class.new do
                   include Definable
@@ -17,75 +18,105 @@ module Definable
       @definition.for_object(@owner)
     end
 
-    it "should add an object if it is in the definition" do
-      @definition.stub!(:complete?).and_return(false)
+    it "should not accept an expected class as parameter" do
       lambda{
-        @definition.add Class.new(@params[1]).new
-      }.should change{@definition.instance_variable_get("@args").size}.by(1)
+        @definition.add @params[0]
+      }.should_not change{@definition.instance_variable_get("@args").size}
     end
 
-    it "should add an object if it is in the definition" do
-      @definition.stub!(:complete?).and_return(false)
-      lambda{
-        @definition.add Class.new(@params[1]).new
-      }.should change{@definition.instance_variable_get("@args").size}.by(1)
+    context "when adding an object" do
+      
+      context "when it gets completed" do
+        
+        it "should call result class' new method" do
+          @result_class.should_receive(:new).once
+          add_to_definition @definition, *@ready_params
+        end
+
+        it "should be complete after adding a all the expected params" do
+          add_to_definition @definition, *@ready_params
+          @definition.should be_complete
+        end
+      end
+
+      it "should call complete? when adding an object" do
+        @definition.should_receive(:complete?).at_least(1).and_return(false)
+        @definition.add @ready_params[1]
+      end
+
+      it "should not call add_object if definition is complete" do
+        @definition.should_receive(:complete?).at_least(1).and_return(true)
+        @definition.should_not_receive(:add_object)
+        @definition.add @ready_params[1]
+      end
     end
 
-    it "should call :add_object if it is not complete" do
-      @definition.should_receive(:add_object).twice
-      add_to_definition @definition, *@params
+    context "when complete" do
+      it "should generate an object" do
+        add_to_definition @definition, *@ready_params
+        @definition.generate.should_not be_nil
+      end
+      
+      context "when complete" do
+
+        before(:each) do
+          @definition.stub!(:complete?).and_return(true)
+        end
+        
+        it "should not add an object even if it is in the definition" do
+          lambda{
+            @definition.add @ready_params[0]
+          }.should_not change{@definition.instance_variable_get("@args").size}
+        end
+      end
     end
 
-    it "should be complete after adding a plane and a point" do
-      add_to_definition @definition, *@params
-      @definition.should be_complete
+    context "when incomplete" do
+      it "should not generate a class" do
+        @definition.generate.should be_nil
+      end
+      
+      context "when adding an object" do
+
+        before(:each) do
+          @definition.stub!(:complete?).and_return(false)
+        end
+        
+        it "should add an object if it is in the definition" do
+          lambda{
+            @definition.add @ready_params[0]
+          }.should change{@definition.instance_variable_get("@args").size}.by(1)
+        end
+        
+        it "should call :add_object if it is not complete" do
+          @definition.should_receive(:add_object).twice
+          add_to_definition @definition, *@ready_params
+        end
+      end
     end
 
-    it "should call complete? when adding an object" do
-      @definition.should_receive(:complete?).at_least(1).and_return(false)
-      @definition.add @params[1].new
-    end
+    describe "#can_be_an_arg?" do
+      it "should consider both simple and tagged arguments as arguments" do
+        Definition.should_receive(:can_be_a_simple_arg?).and_return(false)
+        Definition.should_receive(:can_be_a_tagged_arg?).and_return(true)
+        Definition.send :can_be_an_arg?, [@result_class, :tag]
+      end
 
-    it "should not call add_object if definition is complete" do
-      @definition.should_receive(:complete?).at_least(1).and_return(true)
-      @definition.should_not_receive(:add_object)
-      @definition.add @params[1].new
-    end
+      it "should accept a class" do
+        Definition.send(:can_be_an_arg?, @result_class).should == true
+        Definition.send(:can_be_an_arg?, Class.new).should == true
+      end
 
-    it "should call result class' new method when completed" do
-      @result_class.should_receive(:new).once
-      add_to_definition @definition, *@params
-    end
-
-    it "should generate an object" do
-      add_to_definition @definition, *@params
-      @definition.generate.should_not be_nil
-    end
-
-    it "should not generate a class if it is not complete" do
-      @definition.generate.should be_nil
-    end
-
-    it "should consider both simple and tagged arguments as arguments" do
-      Definition.should_receive(:can_be_a_simple_arg?).and_return(false)
-      Definition.should_receive(:can_be_a_tagged_arg?).and_return(true)
-      Definition.send :can_be_an_arg?, [@result_class, :tag]
-    end
-
-    it "should consider a class an arg" do
-      Definition.send(:can_be_an_arg?, @result_class).should == true
-      Definition.send(:can_be_an_arg?, Class.new).should == true
-    end
-
-    it "should consider a class plus a tag an arg" do
-      Definition.send(:can_be_an_arg?, [@result_class, :tag]).should == true
-      Definition.send(:can_be_an_arg?, [Class.new, :tag]).should == true
+      it "should accept a class plus a tag" do
+        Definition.send(:can_be_an_arg?, [@result_class, :tag]).should == true
+        Definition.send(:can_be_an_arg?, [Class.new, :tag]).should == true
+      end
     end
 
     # Methods
-    def add_to_definition(definition, *classes)
-      classes.each do |klazz|
-        definition.add Class.new(klazz).new
+    def add_to_definition(definition, *obj)
+      obj.each do |o|
+        definition.add o
       end
     end
   end
